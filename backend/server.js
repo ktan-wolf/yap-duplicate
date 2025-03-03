57fd0ce5693313cffdf81e12acb2dcc9b6466532
ktan-wolf <ktan247610@gmial.com>
2025-03-03 14:56:20 +0530

    domain

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin:["http://localhost:5173", "yap-duplicate.vercel.app"],
     credentials: true // Allow cookies to be sent
 }));
 
