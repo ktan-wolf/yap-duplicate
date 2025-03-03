9fdb1b4c27173455a9c440cdc27cf828d209f419
ktan-wolf <ktan247610@gmial.com>
2025-03-03 17:15:18 +0530

    done

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const protectRoute = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        const token = req.cookies.token;
         if(!token) {
             return res.status(401).json({message: "No token, authorization denied"});
         }
